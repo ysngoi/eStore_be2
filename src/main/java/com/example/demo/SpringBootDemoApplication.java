50c68b6b67fb9d11b333492be3c38186e95be381
yewsin <ysngoi@hotmail.com>
2025-06-23 08:26:17 +0800

    commit

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -2,13 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages="com.example.demo")
-@EntityScan("com.example.demo.model")
 public class SpringBootDemoApplication extends SpringBootServletInitializer {
 	
     public static void main(String[] args) {
